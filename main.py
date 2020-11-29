48de4aa76f21ab44b9d9b7954c43c786a1bd5719
konradclapa <konradclapa@gmail.com>
2020-11-29 10:06:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    name = os.environ.get('NAME', 'Youtube2')
+    name = os.environ.get('NAME', 'World')
     return 'Hello {}!'.format(name)
 
 
